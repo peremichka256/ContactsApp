fa1f88c3935457f6039ec0fae0aad9f76dfc7eef
peremichka256 <peremichka256@gmail.com>
2020-11-06 17:01:34 +0700

    Добавлена библиотека Newtonsoft JSON.NET. Реализован класс сериализации ProjectManager

--- ContactsApp/ContactsApp/ProjectManager.cs
+++ ContactsApp/ContactsApp/ProjectManager.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ContactsApp
 {
     /// <summary>
-    /// Класс менеджер проекта, пока не реализован, но после прровери основной логики будет
-    /// осуществлять сериализацию
+    /// Класс менеджер проекта, осуществляtт сериализацию списка контактов
     /// </summary>
-    public class ProjectManager
+    public static class ProjectManager
     {
+        /// <summary>
+        /// Сохраняет объект <see cref="Project"/>
+        /// </summary>
+        /// <param name="project">Сохраняемый объект</param>
+        public static void SaveToFile(Project project)
+        {
+            Directory.CreateDirectory(@"c:\My Documents\ContactsAppData");
+            JsonSerializer jsonSerializer = new JsonSerializer();
+
+            using (StreamWriter stream
+                = new StreamWriter(@"c:\My Documents\ContactsAppData\Contacts.txt"))
+            using (JsonWriter writer = new JsonTextWriter(stream))
+            {
+                jsonSerializer.Serialize(writer, project);
+            }
+
+            
+        }
+
+        /// <summary>
+        /// Загружает из файла объект <see cref="Project"/>
+        /// </summary>
+        /// <returns>Загруженный объект <see cref="Project"/></returns>
+        public static Project LoadFromFile()
+        {
+            Project project = null;
+
+            JsonSerializer jsonSerializer = new JsonSerializer();
+
+            using (StreamReader stream
+                = new StreamReader(@"c:\My Documents\ContactsAppData\Contacts.txt"))
+            using (JsonReader reader = new JsonTextReader(stream))
+            {
+                project = jsonSerializer.Deserialize<Project>(reader);
+            }
+
+            return project;
+        }
     }
 }
--- ContactsApp/ContactsAppUI/Form1.cs
+++ ContactsApp/ContactsAppUI/Form1.cs
@@ -17,69 +17,11 @@ namespace ContactsAppUI
         {
             var newProjectContacts = new Project();
 
-            try
-            {
-                var firstWrongContact = new Contact("белоУс", "Глеб", new PhoneNumber(89237574394),
-                new DateTime(2001, 04, 11), "asd@mail.ru", "kek");
-            }
-            catch(Exception)
-            {
-                //Тут ошибка в первой цифре номера
-            }
+            //Ранее были записаны данные, они успешно загружаются в переменную
+            newProjectContacts = ProjectManager.LoadFromFile();
 
-            try
-            {
-                var firstWrongContact = new Contact("белоУс", "Глеб", new PhoneNumber(89237574394),
-                new DateTime(2001, 04, 11), "asd@mail.ru", "kek");
-            }
-            catch (Exception)
-            {
-                //Тут ошибка в количестве цифр номера
-            }
-
-            try
-            {
-                var firstWrongContact = new Contact("белоУс", "Глеб", new PhoneNumber(79237574394),
-                new DateTime(1899, 04, 11), "asd@mail.ru", "ass");
-            }
-            catch (Exception)
-            {
-                //Тут ошибка в дате рождения
-            }
-
-            try
-            {
-                var firstWrongContact = new Contact("белоУс", "Глеб", new PhoneNumber(79237574394),
-                new DateTime(2021, 04, 11), "asd@mail.ru", "kek");
-            }
-            catch (Exception)
-            {
-                //Тут ошибка в дате рождения
-            }
-
-            try
-            {
-                var firstWrongContact = new Contact("белоУс", "Глеб", new PhoneNumber(79237574394),
-                new DateTime(2001, 04, 11),
-                "asd@mail.ruasd@mail.ruasd@mail.ruasd@mail.ruasd@mail.ru", "kek");
-            }
-            catch (Exception)
-            {
-                //Тут ошибка в количестве символов в почте
-            }
-
-            var myContact = new Contact("белоУс", "Глеб", new PhoneNumber(79237574394),
-                new DateTime(2001, 04, 11), "asd@mail.ru", "kek");
-
-            var myFrendContact = new Contact("Иван", "иванов", new PhoneNumber(79237574994),
-                new DateTime(2001, 04, 11), "asd@mail.ru", "kek");
-
-            //Контакты заносятся в список, посмотрено через отладку
-            newProjectContacts.Contacts.Add(myContact);
-            newProjectContacts.Contacts.Add(myFrendContact);
-
-            //проверка метода клонирования
-            var myContactClone = myContact.Clone();
+            //Загруженные данные успешно сохраняются
+            ProjectManager.SaveToFile(newProjectContacts);
 
             InitializeComponent();
         }
